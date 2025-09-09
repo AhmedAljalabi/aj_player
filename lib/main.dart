5a636d14fe710cacde80bfc4ad6cef9580521bc2
AhmedAljalabi <ahmedforcode@gmail.com>
2025-09-09 14:24:46 +0300

    create apk file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:aj_player/home_screen.dart';
-import 'package:aj_player/bootstrap.dart';  // bootstrapping dependencies
+import 'package:aj_player/bootstrap.dart'; 
+import 'package:just_audio/just_audio.dart';
+import 'package:audio_service/audio_service.dart';
+import 'package:audio_session/audio_session.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:aj_player/services/audio_handler.dart'; // bootstrapping dependencies
 
+// This is the global audio handler instance.
+late AudioPlayerHandler _audioHandler;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();  // required for async init
   await AppBootstrap.init();                 // sets up permissions, Hive, prefs
